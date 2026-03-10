674870ec3c426372cf9ef301d18c04fe4c0840d7
Cassandra Santillano <casantillano@csu.fullerton.edu>
2026-03-10 14:15:25 -0700

    updated Event entity

--- src/main/java/com/example/event_ticketing/entity/Event.java
+++ src/main/java/com/example/event_ticketing/entity/Event.java
@@ -3,5 +3,34 @@ package com.example.event_ticketing.entity;
 import jakarta.persistence.*;
 import java.util.List;
 
+@Entity
+@Table(name="Event")
 public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer event_id;
+
+    @Column(nullable = false)
+    private String title;
+
+    // date
+    // datetime?... event_date;
+
+    // status
+    // enum ... status;
+
+    @Column(nullable = true)
+    private String description;
+
+    // relationships
+    @OneToMany(mappedBy = "event", cascade = CascadeType.ALL)
+    // maybe change var
+    private List<TicketType> type;
+
+    //check syntax, and match to 3.3 schema
+    @ManyToOne
+    @JoinColumn(name = "organizer_id")
+    private Organizer organizer;
+    @JoinColumn(name = "venue_id")
+    private Venue venue;
 }
--- src/main/java/com/example/event_ticketing/entity/TicketType.java
+++ src/main/java/com/example/event_ticketing/entity/TicketType.java
@@ -3,5 +3,6 @@ package com.example.event_ticketing.entity;
 import jakarta.persistence.*;
 import java.util.List;
 
+
 public class TicketType {
 }
