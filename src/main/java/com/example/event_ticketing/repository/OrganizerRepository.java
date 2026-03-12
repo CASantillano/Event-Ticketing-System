cb268bba427409cbdb830ac294d24d5727cb5a9d
Cassandra Santillano <casantillano@csu.fullerton.edu>
2026-03-11 19:22:32 -0700

    repository files structure

---
+++ src/main/java/com/example/event_ticketing/repository/OrganizerRepository.java
@@ -0,0 +1,12 @@
+package com.example.event_ticketing.repository;
+
+import com.example.event_ticketing.entity.Organizer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface OrganizerRepository extends JpaRepository<Organizer, Integer> {
+    //
+}
---
+++ src/main/java/com/example/event_ticketing/repository/TicketTypeRepository.java
@@ -0,0 +1,10 @@
+package com.example.event_ticketing.repository;
+
+import com.example.event_ticketing.entity.TicketType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TicketTypeRepository extends JpaRepository<TicketType, Integer> {
+    //
+}
