e96b46cdd46f5115d367929e7c1c7f25799376a3
Cassandra Santillano <casantillano@csu.fullerton.edu>
2026-03-15 02:36:28 -0700

    updated EventRepository

--- src/main/java/com/example/event_ticketing/repository/EventRepository.java
+++ src/main/java/com/example/event_ticketing/repository/EventRepository.java
@@ -1,19 +1,21 @@
 package com.example.event_ticketing.repository;
 
 import com.example.event_ticketing.entity.Event;
+import com.example.event_ticketing.enums.EventStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @Repository
 public interface EventRepository extends JpaRepository<Event, Integer> {
-    // create a new event happens in service
-
-    // List all upcoming events
-
     // get event details with ticket types
+    List<Event> findByStatus(EventStatus status);
 
     // get total revenue for an event
+    @Query("SELECT SUM(b.ticket_type.price) FROM Booking b WHERE b.ticket_type.event.event_id = :event_id AND b.payment_status = 'CONFIRMED'")
+    BigDecimal calculateRevenueByEventId(@Param("event_id") Integer event_id);
 }
