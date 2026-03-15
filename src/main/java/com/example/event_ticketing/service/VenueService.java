b54e726fa79187de36822024a286dd974d5e8d42
Cassandra Santillano <casantillano@csu.fullerton.edu>
2026-03-15 14:29:17 -0700

    updated VenueService

--- src/main/java/com/example/event_ticketing/service/VenueService.java
+++ src/main/java/com/example/event_ticketing/service/VenueService.java
@@ -1,6 +1,8 @@
 package com.example.event_ticketing.service;
 
+import com.example.event_ticketing.entity.Venue;
 import com.example.event_ticketing.repository.VenueRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,4 +12,14 @@ public class VenueService {
     private VenueRepository venueRepository;
 
     // create a new venue
+    @Transactional
+    public Venue createVenue(String name, String address, String city, Integer total_capacity){
+        Venue venue = new Venue();
+        venue.setName(name);
+        venue.setAddress(address);
+        venue.setCity(city);
+        venue.setTotal_capacity(total_capacity);
+
+        return venueRepository.save(venue);
+    }
 }
